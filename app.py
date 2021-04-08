96ead23c58db7bd1fca8d279d633a8b3461b1bfd
Jackso88 <andreilukin88@gmail.com>
2021-04-08 17:23:54 +0300

    v.2

--- app.py
+++ app.py
@@ -6,72 +6,21 @@ from flask import Flask, render_template, request, redirect, url_for
 from pygal.style import Style
 
 
-url = 'https://wuhan-coronavirus-api.laeyoung.endpoint.ainize.ai/'
-url += 'jhu-edu/latest?onlyCountries=true'
+url = 'https://covid19-api.org/api/status/'
 a = requests.get(url)
 response_dict = a.json()
 
 
-def get_country_code(country_name):
-    for code, name in COUNTRIES.items():
-        if name == country_name:
-            return code
-        if country_name == 'Russia':
-            return 'ru'
-        elif country_name == 'US':
-            return 'us'
-        elif country_name == 'Libya':
-            return 'ly'
-        elif country_name == 'Venezuela':
-            return 've'
-        elif country_name == 'Bolivia':
-            return 'bo'
-        elif country_name == 'India':
-            return 'in'
-        elif country_name == 'Iran':
-            return 'ir'
-        elif country_name == 'Korea, South':
-            return 'kr'
-        elif country_name == 'Burma':
-            return 'mm'
-        elif country_name == 'Vietnam':
-            return 'vn'
-        elif country_name == 'Taiwan*':
-            return 'tw'
-        elif country_name == 'Tanzania':
-            return 'tz'
-        elif country_name == 'Congo (Brazzaville)':
-            return 'cd'
-        elif country_name == 'Congo (Kinshasa)':
-            return 'cg'
-        elif country_name == 'Czechia':
-            return 'cz'
-    return None
-
-cc, data = [], []
 covid_dict = {}
 
 
 def get_dict(x):
 
-    global cc
-    global data
-    global covid_dict
     global response_dict
+    global covid_dict
 
-    for values in response_dict:
-        for key, value in values.items():
-            cc.append(values['countryregion'])
-            data.append(values[x])
-
-    dic_country = dict(zip(cc, data))
-
-    for key, value in dic_country.items():
-        country_name = key
-        conf = value
-        code = get_country_code(country_name)
-        if code:
-            covid_dict[code] = conf
+    for el in response_dict:
+        covid_dict[el['country'].lower()] = el[x]
 
 app = Flask(__name__)
 par = 'this_should_be_configured'
@@ -81,11 +30,12 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', par)
 @app.route('/')
 def confirmed() -> 'html':
     global covid_dict
-    get_dict('confirmed')
+    get_dict('cases')
     chart = pygal.maps.world.World()
     chart.title = 'COVID 19'
-    chart.add('confirmed', covid_dict)
+    chart.add('cases', covid_dict)
     chart = chart.render_data_uri()
+    print(covid_dict)
     return render_template('confirmed.html', chart=chart)
 
 
